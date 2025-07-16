d96748692c8907b33a673bc8aa86643ac1100435
Aditya Singh <160380902+Adityazzzzz@users.noreply.github.com>
2025-07-16 16:41:42 +0530

    Recursion

--- 6. Recursion/3) CombinationSum.cpp
+++ 6. Recursion/3) CombinationSum.cpp
@@ -28,3 +28,26 @@ vector<vector<int>>combination(vector<int> &arr, int target){
 
 
 // Combination Sum 2
+void subseq(int index,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+    if(taregt==0){
+        ans.push_back(ds);
+        return;
+    }
+
+    for(int i=index;i<arr.size();i++){
+        
+        if(i>index && arr[i]==arr[i-1]) continue;
+        if(arr[index]>target) break;
+        ds.push_back(arr[index]);
+        subseq(index+1,target-arr[index],arr,ans,ds);
+        ds.pop_back();
+    }
+
+}
+vector<vector<int>>combination(vector<int> &arr, int target){
+    sort(arr.begin(),arr.end());
+    vector<vector<int>>ans;
+    vector<int>ds;
+    subseq(0,target,arr,ans,ds);
+    return ans;
+}
