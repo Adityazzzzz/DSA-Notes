0bf609e8257a09a6f6930e4d219dabc6e41b0e74
Aditya Singh <aditya.singh@fenrir-security.com>
2025-08-15 18:53:59 +0530

    Graph

--- 5.Graph/ShortestPathAlgo/25) BellamanFord.cpp
+++ 5.Graph/ShortestPathAlgo/25) BellamanFord.cpp
@@ -27,3 +27,34 @@ vector<int> bellman_ford(int V, vector<vector<int>> &edges, int S) {
     }
     return dist;
 }
+
+
+// Flyord Warshall algo
+// O(n^3)
+void shortest_distance(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == -1) {
+                matrix[i][j] = 1e9;
+            }
+            if (i == j) matrix[i][j] = 0;
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = min(matrix[i][j],matrix[i][k] + matrix[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == 1e9) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+}
