f3f8a7d1ef0fa5a75aea599412f732528e38e00d
Aditya Singh <aditya.singh@fenrir-security.com>
2025-08-19 23:12:03 +0530

    LL

--- 3.LinkedList/18) ReverseinKgroups.cpp
+++ 3.LinkedList/18) ReverseinKgroups.cpp
@@ -15,38 +15,56 @@ class Node{
     }
 };
 
-Node* getkthNode(Node* temp,int k){
-    k = k-1;
-    while(temp != NULL && k>0){
+class Solution {
+private:
+    ListNode* findkthNode(ListNode* temp, int k) {
         k--;
-        temp = temp->next;
-    }
-    return temp;
-}
-
-Node* reverseinKgroups(Node* head,int k){
-    Node* temp = head;
-    Node* prevNode = NULL;
-
-    while(temp != NULL){
-        Node* kthnode = getkthNode(temp,k);
-        if(kthnode == NULL){ //edge case: if kth group is smaller than k
-            if(prevNode) prevNode->next = temp;
-            break;
+        while (temp != NULL && k > 0) {
+            k--;
+            temp = temp->next;
         }
-
-        Node* nextNode = kthnode->next;
-        kthnode->next = nullptr;
-        reverseLL(temp); // basic reverseLL function
-
-        if(temp == head) head = kthnode; // it means its a first group
-        else prevNode->next = kthnode;
-
-        prevNode = temp;
-        temp = nextNode;
+        return temp;
     }
-    return head;
-}
+
+    ListNode* reverseLL(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode* newHead = reverseLL(head->next);
+        ListNode* front = head->next;
+
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+
+        while (temp != NULL) {
+            ListNode* kthnode = findkthNode(temp, k);
+            if (kthnode == NULL) {
+                if (prev) prev->next = temp;
+                break;
+            }
+
+            ListNode* nextNode = kthnode->next;
+            kthnode->next = NULL;
+
+            // reverse current block
+            ListNode* newHead = reverseLL(temp);
+
+            // first group
+            if (temp == head) head = newHead;
+            else prev->next = newHead;
+
+            prev = temp;       // 'temp' is now tail of reversed block
+            temp = nextNode;   // move to next group
+        }
+        return head;
+    }
+};
 
 int main(){
     vector<int>arr = {2,5,8,7};
