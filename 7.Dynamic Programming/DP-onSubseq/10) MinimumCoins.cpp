f5a567bafea39a8a2a90657f0f11eeb52256c8fb
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-09-23 15:55:35 +0530

    DP

--- 7.Dynamic Programming/DP-onSubseq/10) MinimumCoins.cpp
+++ 7.Dynamic Programming/DP-onSubseq/10) MinimumCoins.cpp
@@ -14,3 +14,46 @@ int f(int ind, int T, vector<int> &nums) {
 
     return dp[ind][T] = max(take, notTake);
 }
+
+// tabulation --------------------------------------------------------------------------------------
+int minimumCoins(std::vector<int>& nums, int target) {
+    int n = nums.size();
+    vector<vector<int>> dp(n,vector<int>(target+1, 0));
+
+    for (int T=0;T<=target;T++) {
+        if(T % nums[0] == 0) dp[0][T] = T / nums[0];
+        else dp[0][T] = 1e9;
+    }
+
+    for(int ind=1;ind<n;ind++){
+        for(int T=0;T<=target;T++){
+
+            int notTake = dp[ind-1][T];
+            int take = INT_MAX; 
+            if (nums[ind] <= T) take = 1 + dp[ind][T - nums[ind]];
+            
+            dp[ind][T] = min(take, notTake);
+        }
+    }
+    int ans = dp[n-1][target];
+    if (ans >= 1e9) return -1;
+    return ans;
+}
+
+// space optimization -----------------------------------------------------------------------------------
+int minimumCoinsSpaceOptimized(std::vector<int>& nums,int target) {
+    int n = nums.size();
+    vector<int>dp(target+1,1e9);
+    dp[0] = 0;
+
+    for(int i=0; i<n;++i){
+        for(int j = nums[i]; j<=target; ++j){
+            if(dp[j - nums[i]] != 1e9){
+                dp[j] = min(dp[j], 1 + dp[j - nums[i]]);
+            }
+        }
+    }
+
+    if (dp[target] >= 1e9) return -1;
+    return dp[target];
+}
