89e13982e3c94bf3ab010cbbd9ec26d940582601
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-11-02 10:48:30 +0530

    DP

--- 7.Dynamic Programming/DPonStrings/18) EditDist.cpp
+++ 7.Dynamic Programming/DPonStrings/18) EditDist.cpp
@@ -7,7 +7,7 @@ int f(int i,intj,string &s1,string &s2,vector<vector<int>> &dp){
     if(dp[i][j]!=-1) return dp[i][j];
 
     if(s1[i] == s2[j]) return f(i-1,j-1,s1,s2,dp); 
-    return dp[i][j] = 1 + min(f(i-1,j,s1,s2,dp),min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp))); 
+    return dp[i][j] = 1 + min( f(i-1,j,s1,s2,dp) , min( f(i,j-1,s1,s2,dp), f(i-1,j-1,s1,s2,dp))); 
 }
 // same code after shifting
 int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
