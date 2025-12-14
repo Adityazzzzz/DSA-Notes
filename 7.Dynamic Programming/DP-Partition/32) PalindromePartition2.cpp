dee0e767167a9571c8976c4047df4393dff0a5d0
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-14 14:18:49 +0530

    DP

---
+++ 7.Dynamic Programming/DP-Partition/32) PalindromePartition2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int i, int j, string s) {
+    while (i < j) {
+        if (s[i] != s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+int f(int i, int n, string &str) {
+    if (i == n) return 0;
+    int minCost = INT_MAX;
+    for (int j = i; j < n; j++) {
+        if (isPalindrome(i, j, str)) {
+            int cost = 1 + f(j + 1, n, str);
+            minCost = min(minCost, cost);
+        }
+    }
+    return minCost-1;
+}
+
+// tabulation --------------------------------------------------------------------------------------
+int palindromePartitioning(string str) {
+    int n = str.size();
+    vector<int> dp(n + 1, 0);
+    dp[n] = 0;
+
+    for (int i = n - 1; i >= 0; i--) {
+        int minCost = INT_MAX;
+        // i...j
+        for (int j = i; j < n; j++) {
+            if (isPalindrome(i, j, str)) {
+                int cost = 1 + dp[j + 1];
+                minCost = min(minCost, cost);
+            }
+        }
+        dp[i] = minCost;
+    }
+
+    return dp[0] - 1;
+}
---
+++ 7.Dynamic Programming/DP-Partition/33) MaxSubarr.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int f(int ind, vector<int> &num, int k) {
+    int n = num.size();
+    if (ind == n) return 0;
+    
+    int len = 0;
+    int maxi = INT_MIN;
+    int maxAns = INT_MIN;
+
+    for(int j=ind;j<min(ind + k, n);j++){
+        len++;
+        maxi = max(maxi, num[j]);
+        int sum = len*maxi + f(j+1,num,k);
+        maxAns = max(maxAns,sum);
+    }
+    return maxAns;
+}
+
+//tabulation -------------------------------------------------------------------------------------
+int maximumSubarray(vector<int> &num, int k) {
+    int n = num.size();
+    vector<int> dp(n + 1, 0);
+
+    for (int ind = n - 1; ind >= 0; ind--) {
+        int len = 0;
+        int maxi = INT_MIN;
+        int maxAns = INT_MIN;
+
+        for (int j = ind; j < min(ind + k, n); j++) {
+            len++;
+            maxi = max(maxi, num[j]);
+            int sum = len * maxi + dp[j + 1];
+            maxAns = max(maxAns, sum);
+        }
+
+        dp[ind] = maxAns;
+    }
+
+    return dp[0];
+}
