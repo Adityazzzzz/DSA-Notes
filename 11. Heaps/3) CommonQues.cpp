e5b64e24fd61d1649b2b985b6526ec893af26354
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-31 08:17:34 +0530

    Heap

--- 11. Heaps/3) CommonQues.cpp
+++ 11. Heaps/3) CommonQues.cpp
@@ -60,3 +60,26 @@ bool isHeap(struct Node* root) {
     if (isCBT(root, index, totalCount) && isMaxOrder(root)) return true;
     else return false;
 }
+//---------------------------------------------------------------------------------------------------
+
+// min cost of n ropes
+
+long long minCost(long long arr[], long long n) {
+    // create a min heap
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    for (long long i = 0; i < n; i++) {
+        pq.push(arr[i]);
+    }
+
+    long long cost = 0;
+    while (pq.size() > 1) {
+        long long a = pq.top();
+        pq.pop();
+        long long b = pq.top();
+        pq.pop();
+        long long sum = a + b;
+        cost += sum;
+        pq.push(sum);
+    }
+    return cost;
+}
---
+++ 11. Heaps/4) MergeTwoHeaps.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(vector<int> &arr, int n, int i) {
+    int largest = i;
+    int left  = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+vector<int> mergeHeaps(const vector<int> &a, const vector<int> &b) {
+    vector<int> merged;
+    merged.reserve(a.size() + b.size());
+
+    // copy all elements
+    merged.insert(merged.end(), a.begin(), a.end());
+    merged.insert(merged.end(), b.begin(), b.end());
+
+    int n = merged.size();
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        heapify(merged, n, i);
+    }
+    return merged;
+}
