91570ac1b74402c7c331df233578aef9d205320b
Adityazzzzz <adityasinghrajawat2004@gmail.com>
2026-05-21 10:40:49 +0530

    BS

--- 2.Binary Search/BS - onAnswers/8) Bouquet.cpp
+++ 2.Binary Search/BS - onAnswers/8) Bouquet.cpp
@@ -1,44 +1,35 @@
 #include<iostream>
 using namespace std;
 
-// we have m = (number to bouquet to form) and k = (no. of adjacent flowers to take to make one bouquet)
-
-int possible(int *arr,int day,int m,int k){
-    int cnt=0;
-    int numB=0;
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]<=day){
-            cnt++;
+class Solution {
+public:
+    bool func(vector<int>&bloomDay, int day, int m, int k) {
+        int cnt = 0;
+        for(int x : bloomDay) {
+            if (x <= day) {
+                cnt++;
+                if (cnt == k) {
+                    m--;
+                    cnt = 0;
+                }
+            }
+            else cnt = 0;
         }
-        else{
-            numB=numB+(cnt/k);
-        }
+        return m <= 0;
     }
-    numB=numB+(cnt/k);
-    return numB>=m;
-}
-int koko(int *arr,int n,int h){
-    int low=*min_element(arr,arr+n), high=*max_element(arr,arr+n);
-    int ans=0;
-    while(low<=high){
-        int mid=(low+high)/2;
-        
-        if(possible(arr,mid,m,k)==true){
-            ans=mid;
-            high=mid-1
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int l = 1;
+        int r = *max_element(bloomDay.begin(), bloomDay.end());
+        int result = -1;
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+            if (func(bloomDay, mid, m, k)) {
+                result = mid;
+                r = mid-1;
+            }
+            else l = mid+1;
         }
-        else low=mid+1;
+        return result;
     }
-    return low;// or return ans;
-}
-
-int main(){
-    int n=4;
-    int minhrs=8;
-    int arr[]={3,4,6,11};
-    cout<<koko(arr,n,minhrs)<<endl;
-    return 0;
-}
-
-
-
+};
