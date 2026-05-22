631081953eb9205d9a8a5aa0ff8ae9497121d0ce
Adityazzzzz <adityasinghrajawat2004@gmail.com>
2026-05-22 23:03:48 +0530

    BS

---
+++ 2.Binary Search/BS - 2Darr/20) Searchin2D-2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+bool searchElement(int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    int row = 0;
+    int col = m-1;
+    while (row < n && col >= 0) {
+        if (matrix[row][col] == target) {
+            return true; 
+        } 
+        else if (matrix[row][col] < target) {
+            row++;
+        } 
+        else {
+            col--;
+        }
+    }
+    return false;
+}
