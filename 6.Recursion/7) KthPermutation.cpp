1426fac06f27f7b66b7debe83c0f077c5387fb00
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-11 07:53:34 +0530

    Recursion

--- 6.Recursion/7) KthPermutation.cpp
+++ 6.Recursion/7) KthPermutation.cpp
@@ -12,11 +12,14 @@ string getPermutation(int n, int k) {
     string ans = "";
     k = k - 1;
     while (true) {
-        ans = ans + to_string(numbers[k / fact]);
-        numbers.erase(numbers.begin() + k / fact);
+        int index = k / fact;
+        ans = ans + to_string(numbers[index]);
+        numbers.erase(numbers.begin() + index);
+
         if (numbers.size() == 0) {
             break;
         }
+        
         k = k % fact;
         fact = fact / numbers.size();
     }
