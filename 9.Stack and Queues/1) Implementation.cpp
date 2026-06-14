868f09f1c13607e0c3b3e13b42a86914fd17748a
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-15 02:47:55 +0530

    S&Q

--- 9.Stack and Queues/1) Implementation.cpp
+++ 9.Stack and Queues/1) Implementation.cpp
@@ -14,84 +14,77 @@ public:
             q.pop();
         }
     }
-    int pop() {
+    int pop(){
         int top = q.front();
         q.pop();
         return top;
     }
-    int top() {
+    int top(){
         return q.front();
     }
-    bool empty() {
+    bool empty(){
         return q.empty();
     }
 };
 
 //--------------------------------------------------------------------------------------------------
 // 2. Implement Queue using 2 stacks - COSTLY PUSH - easy 
-class Queue {
+class Queue{
     stack<int> s1, s2;
 public:
-    void push(int x) {
-        while (!s1.empty()) {
+    void push(int x){
+        while(!s1.empty()){
             s2.push(s1.top());
             s1.pop();
         }
         s1.push(x);
-        while (!s2.empty()) {
+        while(!s2.empty()){
             s1.push(s2.top());
             s2.pop();
         }
     }
-    void pop() {
-        if (s1.empty()) {
-            return;
-        }
+    void pop(){
+        if(s1.empty()) return;
         s1.pop();
     }
-    int front() {
-        if (s1.empty()) return -1;
+    int front(){
+        if(s1.empty()) return -1;
         return s1.top();
     }
-    bool empty() {
+    bool empty(){
         return s1.empty();
     }
 };
 
 //---------------------------------------------------------------------------------------------------
-// 3. Implement Queue in O(1) / Implement Queue using 2 stacks - COSTLY POP;
-class MyQueue {
-    stack<int>st1;
-    stack<int>st2;
+// 3. Implement Queue in O(1) / Implement Queue using 2 stacks - COSTLY POP - interview;
+class Queue{
+    stack<int> s1, s2;
 public:
-    MyQueue(){   
-    }
     void push(int x){
-        st1.push(x);
+        s1.push(x);
     }
-
-    int pop(){
-        while(st2.empty()){
-            while(!st1.empty()){
-                st2.push(st1.top());
-                st1.pop();
+    void pop(){
+        if(empty()) return;
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
             }
         }
-        int top = st2.top();
-        st2.pop();
-        return top;
+        s2.pop();
     }
-    
-    int peek(){
-        if (st2.empty()) {
-            while (!st1.empty()) {
-                st2.push(st1.top());
-                st1.pop();
+    int front(){
+        if(empty()) return -1;
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
             }
         }
-        return st2.top();
+        return s2.top();
     }
     bool empty(){
-        return st1.empty() && st2.empty();   
+        return s1.empty() && s2.empty();
     }
 };
