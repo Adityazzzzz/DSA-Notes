e1d9bce37ba78e7be063ec2243b92902d2b6ca94
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-16 09:03:29 +0530

    S&Q

---
+++ 9.Stack and Queues/6) SumofSubsetArr.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+vector<int> getPSE(vector<int>& arr){
+    int n = arr.size();
+    vector<int> psee(n);
+    stack<int> st;
+    for(int i = 0; i < n; i++){
+        while(!st.empty() && arr[st.top()] > arr[i])
+            st.pop();
+
+        psee[i] = st.empty() ? -1 : st.top();
+        st.push(i);
+    }
+    return psee;
+}
+
+vector<int> getNSE(vector<int>& arr){
+    int n = arr.size();
+    vector<int> nse(n);
+    stack<int> st;
+    for(int i = n - 1; i >= 0; i--){
+        while(!st.empty() && arr[st.top()] >= arr[i])
+            st.pop();
+
+        nse[i] = st.empty() ? n : st.top();
+        st.push(i);
+    }
+    return nse;
+}
+int sumSubarrayMins(vector<int>& arr){
+    int n = arr.size();
+    vector<int> psee = getPSE(arr);
+    vector<int> nse = getNSE(arr);
+    long long ans = 0;
+
+    for(int i = 0; i < n; i++){
+        long long left = i - psee[i];
+        long long right = nse[i] - i;
+
+        ans =(ans + (arr[i] * left % MOD) * right) % MOD;
+    }
+    return ans;
+}
