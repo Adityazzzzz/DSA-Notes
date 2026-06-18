e28b4c2220bc3abca565c0af7ff52b3d414d6aa7
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-19 03:01:04 +0530

    Heap

--- 11.Heaps/2) HeapifyAlgo.cpp
+++ 11.Heaps/2) HeapifyAlgo.cpp
@@ -6,8 +6,8 @@ using namespace std;
 // implemented max-heap
 void heapify(int arr[], int n, int i) {
     int largest = i;
-    int left = 2 * i;
-    int right = 2 * i + 1;
+    int left = 2*i;
+    int right = 2*i+1;
 
     if (left<n && arr[largest]<arr[left]) largest = left;
     if (right<n && arr[largest]<arr[right]) largest = right;
