33057f40f1786a9dcc97818b44edb6d43aa3b34c
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-20 03:14:18 +0530

    Heap

--- 11.Heaps/3) CommonQues.cpp
+++ 11.Heaps/3) CommonQues.cpp
@@ -60,11 +60,9 @@ bool isHeap(struct Node* root){
 //---------------------------------------------------------------------------------------------------
 
 // min cost of n ropes
-
 long long minCost(long long arr[], long long n){
-    // create a min heap
     priority_queue<long long, vector<long long>, greater<long long>> pq;
-    for(long long i = 0;i < n;i++){
+    for(long long i=0;i<n;i++){
         pq.push(arr[i]);
     }
 
@@ -72,8 +70,10 @@ long long minCost(long long arr[], long long n){
     while(pq.size() > 1){
         long long a = pq.top();
         pq.pop();
+
         long long b = pq.top();
         pq.pop();
+        
         long long sum = a + b;
         cost += sum;
         pq.push(sum);
