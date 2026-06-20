151dc78b235e57eb8988457bf126f82e86bf3981
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-20 09:04:19 +0530

    Heap

--- 11.Heaps/4) MergeTwoHeaps.cpp
+++ 11.Heaps/4) MergeTwoHeaps.cpp
@@ -1,33 +1,31 @@
 #include<iostream>
 using namespace std;
 
-void heapify(vector<int> &arr, int n, int i) {
+void heapify(vector<int> &arr,int n,int i){
     int largest = i;
-    int left  = 2 * i + 1;
-    int right = 2 * i + 2;
+    int left  = 2*i+1;
+    int right = 2*i+2;
 
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
+    if(left<n && arr[left]>arr[largest]) largest = left;
+    if(right<n && arr[right]>arr[largest]) largest = right;
 
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
+    if(largest != i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
     }
 }
 
-vector<int> mergeHeaps(const vector<int> &a, const vector<int> &b) {
-    vector<int> merged;
-    merged.reserve(a.size() + b.size());
+vector<int> mergeHeaps(const vector<int> &a,const vector<int> &b){
+    vector<int>ans;
+    ans.reserve(a.size() + b.size());
 
     // copy all elements
-    merged.insert(merged.end(), a.begin(), a.end());
-    merged.insert(merged.end(), b.begin(), b.end());
+    ans.insert(ans.end(),a.begin(),a.end());
+    ans.insert(ans.end(),b.begin(),b.end());
 
-    int n = merged.size();
-    for (int i = n / 2 - 1; i >= 0; --i) {
-        heapify(merged, n, i);
+    int n = ans.size();
+    for(int i=n/2-1;i>=0;i--){
+        heapify(ans,n,i);
     }
-    return merged;
+    return ans;
 }
