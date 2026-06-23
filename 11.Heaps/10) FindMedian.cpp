de3410ac6c96c0752b285f8c218f545408733a5e
Adityazzzzz <adityasinghrajawat2004@gmail.com>
2026-06-23 09:18:07 +0530

    Heap

--- 11.Heaps/10) FindMedian.cpp
+++ 11.Heaps/10) FindMedian.cpp
@@ -1,60 +1,70 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int signum(int a,int b){
     if(a == b) return 0;
-    return (a > b) ? 1 : -1;
+    return(a > b) ? 1 : -1;
 }
-void callMedian(vector<int> &arr,priority_queue<int> &maxi,priority_queue<int,vector<int>,greater<int>> &mini,int &median,int i){
-    
+
+void callMedian(int x,
+                priority_queue<int>& maxi,
+                priority_queue<int,vector<int>,greater<int>>& mini,
+                int& median){
+
     switch(signum(maxi.size(),mini.size())){
+
+        // Both heaps have equal size
         case 0:
-            if(arr[i] > median){
-                mini.push(arr[i]);
+            if(x > median){
+                mini.push(x);
                 median = mini.top();
-            } 
+            }
             else{
-                maxi.push(arr[i]);
+                maxi.push(x);
                 median = maxi.top();
             }
             break;
 
+        // Max heap has one extra element
         case 1:
-            if(arr[i] > median){
-                mini.push(arr[i]);
-            } 
+            if(x > median){
+                mini.push(x);
+            }
             else{
                 mini.push(maxi.top());
                 maxi.pop();
-                maxi.push(arr[i]);
+                maxi.push(x);
             }
+
             median =(maxi.top() + mini.top()) / 2;
             break;
 
+        // Min heap has one extra element
         case -1:
-            if(arr[i] > median){
+            if(x > median){
                 maxi.push(mini.top());
                 mini.pop();
-                mini.push(arr[i]);
-            } 
-            else{
-                maxi.push(arr[i]);
+                mini.push(x);
             }
+            else{
+                maxi.push(x);
+            }
+
             median =(maxi.top() + mini.top()) / 2;
             break;
     }
 }
 
-vector<int> findMedian(vector<int> &arr,int n){
-
+vector<int> findMedian(vector<int>& arr,int n){
     vector<int> ans;
-    priority_queue<int> maxi;// max-heap(lower half)
-    priority_queue<int,vector<int>,greater<int>> mini;// min-heap(upper half)
-    int median = -1;
 
+    priority_queue<int> maxi;
+    priority_queue<int,vector<int>,greater<int>> mini;
+
+    int median = 0;
     for(int i=0;i<n;i++){
-        callMedian(arr,maxi,mini,median,i);
-        ans.push_back(median);  // store current median after each insertion
+        callMedian(arr[i],maxi,mini,median);
+        ans.push_back(median);
     }
     return ans;
 }
