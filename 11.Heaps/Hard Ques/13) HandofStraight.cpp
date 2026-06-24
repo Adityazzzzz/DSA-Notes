cf4816abaac7eab7b7717a3d335d4607ea4daa48
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-24 07:43:56 +0530

    Heap

--- 11.Heaps/Hard Ques/13) HandofStraight.cpp
+++ 11.Heaps/Hard Ques/13) HandofStraight.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-public:
-    bool isNStraightHand(vector<int>& hand,int size){
-        int n = hand.size();
-        if(n % size != 0) return false;
+bool isNStraightHand(vector<int>& hand,int size){
+    int n = hand.size();
+    if(n%size!=0) return false;
 
-        map<int,int> mpp;
-        for(int &it : hand){
-            mpp[it]++;
-        }
+    map<int,int>mpp;
+    for(int &it:hand){
+        mpp[it]++;
+    }
 
-        while(!mpp.empty()){
-            int curr = mpp.begin()->first;
+    while(!mpp.empty()){
+        int curr = mpp.begin()->first;
 
-            for(int i = 0; i < size; i++){
-                if(mpp[curr + i] == 0) return false;
-                mpp[curr + i]--;
+        for(int i = 0; i < size; i++){
+            if(mpp[curr+i]==0) return false;
+            mpp[curr+i]--;
                 
-                if(mpp[curr + i] == 0){
-                    mpp.erase(curr + i);
-                }
+            if(mpp[curr+i]==0){
+                mpp.erase(curr+i);
             }
         }
-        return true;
     }
-};
+    return true;
+}
