2046f9e6661ba9061026de7aa35b9aeb8bba9748
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-24 07:57:38 +0530

    Heap

--- 11.Heaps/Hard Ques/13) HandofStraight.cpp
+++ 11.Heaps/Hard Ques/13) HandofStraight.cpp
@@ -13,10 +13,10 @@ bool isNStraightHand(vector<int>& hand,int size){
     while(!mpp.empty()){
         int curr = mpp.begin()->first;
 
-        for(int i = 0; i < size; i++){
+        for(int i=0;i<size;i++){
             if(mpp[curr+i]==0) return false;
+
             mpp[curr+i]--;
-                
             if(mpp[curr+i]==0){
                 mpp.erase(curr+i);
             }
