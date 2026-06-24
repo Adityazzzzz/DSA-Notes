df5430da1fc139323f01bd5ea80815cae0dca48c
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-06-24 07:35:13 +0530

    Heap

--- 11.Heaps/9) SmallestRangeinKlists.cpp
+++ 11.Heaps/9) SmallestRangeinKlists.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-vector<int> smallestRange(vector<vector<int>>& nums) {
+vector<int> smallestRange(vector<vector<int>>& nums){
     vector<int> ans(2, INT_MAX);
     int minRange = INT_MAX;
     
@@ -10,25 +10,25 @@ vector<int> smallestRange(vector<vector<int>>& nums) {
     int maxVal = INT_MIN;
     
     int k = nums.size();
-    for (int i = 0; i < k; i++) {
-        if (!nums[i].empty()) {
+    for(int i = 0; i < k; i++){
+        if(!nums[i].empty()){
             minHeap.emplace(nums[i][0], i, 0);
             maxVal = max(maxVal, nums[i][0]);
         }
     }
     
-    while (minHeap.size() == k) {
+    while(minHeap.size() == k){
         auto [minVal, listIdx, elemIdx] = minHeap.top();
         minHeap.pop();
         
         // Update smallest range
-        if (maxVal-minVal < minRange) {
+        if(maxVal-minVal < minRange){
             minRange = maxVal - minVal;
-            ans = {minVal, maxVal};
+            ans ={minVal, maxVal};
         }
         
         // Push next from same list
-        if (elemIdx+1 < nums[listIdx].size()) {
+        if(elemIdx+1 < nums[listIdx].size()){
             int nextVal = nums[listIdx][elemIdx+1];
             minHeap.emplace(nextVal,listIdx,elemIdx+1);
             maxVal = max(maxVal,nextVal);
--- 11.Heaps/Hard Ques/12) TaskScheduler.cpp
+++ 11.Heaps/Hard Ques/12) TaskScheduler.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int leastInterval(vector<char>& tasks, int n) {
-    unordered_map<char, int> freq;
-    for (char task : tasks) {
-        freq[task]++;
+int leastInterval(vector<char>& tasks,int n){
+    
+    unordered_map<char,int> freq;
+    for(char it:tasks){
+        freq[it]++;
     }
+
     priority_queue<int> maxHeap;
-    for (auto& entry : freq) {
-        maxHeap.push(entry.second);
+    for(auto& it:freq){
+        maxHeap.push(it.second);
     }
 
     int time = 0;
-    while (!maxHeap.empty()) {
+    while(!maxHeap.empty()){
         vector<int> temp;
-        int cycle = n + 1;
+        int cycle = n+1;
         int i = 0;
 
-        while (i < cycle && !maxHeap.empty()) {
+        while(i<cycle && !maxHeap.empty()){
             int cnt = maxHeap.top();
             maxHeap.pop();
+            
             cnt--;
-            if (cnt > 0) {
-                temp.push_back(cnt);
-            }
-
+            if(cnt>0) temp.push_back(cnt);
             time++;
             i++;
         }
         
-        for (int val : temp) {
-            maxHeap.push(val);
+        for(int it:temp){
+            maxHeap.push(it);
         }
 
-        if (maxHeap.empty()) break;
-        time += (cycle - i);
+        if(maxHeap.empty()) break;
+        time +=(cycle - i);
     }
     return time;
 }
