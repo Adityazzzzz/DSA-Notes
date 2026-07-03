2481db728255b5f46a40b8dd9ac54665a2ffb8f2
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-07-04 04:02:40 +0530

    S&Q

--- 9.Stack and Queues/12) StockSpanner.cpp
+++ 9.Stack and Queues/12) StockSpanner.cpp
@@ -11,15 +11,15 @@ class StockSpanner{
         index = -1;
         st.clear();
     }
+    
     int next(int val){
         index = index + 1;
         while(!st.empty() && st.top()<=val){
             st.pop();
         }
-
         ans = index - (st.empty() ? -1 : st.top().second)
         
-        st.({val,index});
+        st.push({val,index});
         return ans;
     }
 }
--- 9.Stack and Queues/14) CelebrityProblem.cpp
+++ 9.Stack and Queues/14) CelebrityProblem.cpp
@@ -1,24 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int func(vector<vector<int>>& matrix) {
+int func(vector<vector<int>>& matrix){
     int n = matrix.size();
-    int top = 0, down = n - 1;
+    int top = 0, down = n-1;
 
-    while (top < down) {
-        if (matrix[top][down] == 1) {
-            top++;
-        } 
-        else {
-            down--;
-        }
+    while(top < down){
+        if(matrix[top][down] == 1) top++;
+        else down--;
     }
 
     int candidate = top;
 
-    for (int i = 0; i < n; i++) {
-        if (i == candidate) continue;
-        if (matrix[candidate][i] == 1 || matrix[i][candidate] == 0){
+    for(int i=0;i<n;i++){
+        if(i==candidate) continue;
+        if(matrix[candidate][i]==1 || matrix[i][candidate]==0){
             return -1;
         }
     }
