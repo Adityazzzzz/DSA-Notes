95abbd3fdf1e0c316a9098c71da3c22ffaff01b8
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-07-09 04:06:50 +0530

    BT

--- 4.BinaryTree/10) MaxWidth.cpp
+++ 4.BinaryTree/10) MaxWidth.cpp
@@ -15,29 +15,29 @@ class Node{
     }
 };
 
-int widthOfBinaryTree(TreeNode* root) {
-        if (!root) return 0;
+int widthOfBinaryTree(TreeNode* root){
+        if(!root) return 0;
         int ans = 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
 
-        while (!q.empty()) {
+        while(!q.empty()){
             int size = q.size();
             int mini = q.front().second;
-            int first, last;
+            int first,last;
 
-            for (int i = 0; i < size; i++) {
+            for(int i=0;i<size;i++){
                 int it = q.front().second - mini;
                 TreeNode* temp = q.front().first;
                 q.pop();
 
-                if (i == 0) first = it;
-                if (i == size - 1) last = it;
+                if(i==0) first = it;
+                if(i==size-1) last = it;
 
-                if (temp->left) q.push({temp->left, it * 2 + 1});
-                if (temp->right) q.push({temp->right, it * 2 + 2});
+                if(temp->left) q.push({temp->left,it*2 +1});
+                if(temp->right) q.push({temp->right,it*2 +2});
             }
-            ans = max(ans, last - first + 1);
+            ans = max(ans,last - first + 1);
         }
         return ans;
 }
