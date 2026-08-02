9fbb4b91c454e8d3f624c70ece9cf75b7f658c0d
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-02 20:30:42 +0530

    Graph

--- 5.Graph/ShortestPathAlgo/25) BellamanFord.cpp
+++ 5.Graph/ShortestPathAlgo/25) BellamanFord.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 
 
-vector<int> bellman_ford(int V, vector<vector<int>> &edges, int S) {
-    vector<int> dist(V, 1e8);
+vector<int> bellman_ford(int V,vector<vector<int>> &edges,int S){
+    vector<int> dist(V,1e8);
     dist[S] = 0;
-    for (int i = 0; i < V - 1; i++) {
-        for (auto it : edges) {
+    for(int i = 0; i < V - 1; i++){
+        for(auto it : edges){
             int u = it[0];
             int v = it[1];
             int wt = it[2];
-            if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+            if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
                 dist[v] = dist[u] + wt;
             }
         }
     }
 
     // Nth relaxation to check negative cycle
-    for (auto it : edges) {
+    for(auto it : edges){
         int u = it[0];
         int v = it[1];
         int wt = it[2];
-        if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
-            return {-1};
+        if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
+            return{-1};
         }
     }
     return dist;
@@ -31,28 +31,28 @@ vector<int> bellman_ford(int V, vector<vector<int>> &edges, int S) {
 
 // Flyord Warshall algo
 // O(n^3)
-void shortest_distance(vector<vector<int>> &matrix) {
+void shortest_distance(vector<vector<int>> &matrix){
     int n = matrix.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == -1) {
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j] == -1){
                 matrix[i][j] = 1e9;
             }
-            if (i == j) matrix[i][j] = 0;
+            if(i == j) matrix[i][j] = 0;
         }
     }
 
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+    for(int k = 0; k < n; k++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 matrix[i][j] = min(matrix[i][j],matrix[i][k] + matrix[k][j]);
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == 1e9) {
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j] == 1e9){
                 matrix[i][j] = -1;
             }
         }
