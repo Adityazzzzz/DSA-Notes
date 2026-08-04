90aed81fe5d24f8662b11381d4c5a4ddd11b14e3
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-04 08:47:59 +0530

    Graph

--- 5.Graph/ShortestPathAlgo/25) BellamanFord.cpp
+++ 5.Graph/ShortestPathAlgo/25) BellamanFord.cpp
@@ -5,7 +5,8 @@ using namespace std;
 vector<int> bellman_ford(int V,vector<vector<int>> &edges,int S){
     vector<int> dist(V,1e8);
     dist[S] = 0;
-    for(int i = 0; i < V - 1; i++){
+
+    for(int i=0;i<V-1;i++){
         for(auto it : edges){
             int u = it[0];
             int v = it[1];
