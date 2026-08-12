f1f9b57ff2ffd05e6ad87fa11ab48ef64b22947a
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-12 19:25:36 +0530

    Array

--- 1.Arrays/18) RotateMatrix.cpp
+++ 1.Arrays/18) RotateMatrix.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
-
-// O(N/2 * N/2) + O(N*N/2)
+// O(N/2*N/2) + O(N*N/2)
 void rotatematrix(vector<int> &arr,int n){
-    for(int i=0;i<n-1;i++){ // till second last ele
+    for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             swap(arr[i][j],arr[j][i]);
         }
@@ -13,10 +11,4 @@ void rotatematrix(vector<int> &arr,int n){
     for(int i=0;i<n;i++){
         reverse(arr[i].begin(),arr[i].end());
     }
-}
-
-int main(){
-    // input a 2D matrix 
-    cout<<rotatematrix(arr,n)<<endl;;
-    return 0;
 }
