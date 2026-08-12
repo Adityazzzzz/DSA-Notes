22fe2ea2a80cd5c10ba15d9943e159b3a2397c17
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-13 00:11:32 +0530

    Heap

--- 11.Heaps/8) MergeKsortedLL.cpp
+++ 11.Heaps/8) MergeKsortedLL.cpp
@@ -4,23 +4,23 @@ using namespace std;
 class Node{
     int val;
     Node *next;
-    Node(int x) : val(x), next(nullptr){}
+    Node(int x) : val(x),next(nullptr){}
 };
 
 class Compare{
-    bool operator()(const pair<Node*, int> &a, const pair<Node*, int> &b){
+    bool operator()(const pair<Node*,int> &a,const pair<Node*,int> &b){
         return a.first->val > b.first->val;
     }
 };
 
 Node* mergeKLists(vector<Node*> &lists){
-    priority_queue<pair<Node*, int>, vector<pair<Node*, int>>, Compare> minHeap;
+    priority_queue<pair<Node*,int>,vector<pair<Node*,int>>,Compare> minHeap;
     
     // Initialize heap with head of each list
     int k = lists.size();
-    for(int i = 0; i < k; i++){
+    for(int i=0;i<k;i++){
         if(lists[i]){
-            minHeap.emplace(lists[i], i);
+            minHeap.emplace(lists[i],i);
         }
     }
     
@@ -28,7 +28,7 @@ Node* mergeKLists(vector<Node*> &lists){
     Node *tail = &dummy;
     
     while(!minHeap.empty()){
-        auto [node, listIdx] = minHeap.top();
+        auto [node,listIdx] = minHeap.top();
         minHeap.pop();
         
         tail->next = node;
@@ -36,7 +36,7 @@ Node* mergeKLists(vector<Node*> &lists){
         
         // Add next node from same list
         if(node->next){
-            minHeap.emplace(node->next, listIdx);
+            minHeap.emplace(node->next,listIdx);
         }
     }
     
