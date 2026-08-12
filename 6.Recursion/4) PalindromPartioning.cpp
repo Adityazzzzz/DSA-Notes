05ab060bed0576e777350f5f1de40502515e0d63
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-12 22:50:52 +0530

    Recursion

--- 6.Recursion/4) PalindromPartioning.cpp
+++ 6.Recursion/4) PalindromPartioning.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-bool isPalindrome(string &str) {
+bool isPalindrome(string &str){
     int len = str.length();
     int right = len - 1;
     int left = 0;
-    while(left <= right) {
+    while(left <= right){
         if(str[left] != str[right]) return false;
         left++;
         right--;
@@ -13,24 +13,24 @@ bool isPalindrome(string &str) {
     return true;
 }
 
-void solve(int idx, string &str, vector<string> &ds, vector<vector<string>> &ans) {
-    if(idx == str.length()) {
+void solve(int idx,string &str,vector<string> &ds,vector<vector<string>> &ans){
+    if(idx == str.length()){
         ans.push_back(ds);
         return;
     }
 
-    for(int i = idx; i < str.length(); i++) {
-        string newStr = str.substr(idx, i - idx + 1);
-        if(isPalindrome(newStr)) {
+    for(int i=idx;i<str.length();i++){
+        string newStr = str.substr(idx,i-idx+1);
+        if(isPalindrome(newStr)){
             ds.push_back(newStr);
-            solve(i + 1, str, ds, ans);
+            solve(i + 1,str,ds,ans);
             ds.pop_back();
         }
     } 
 }
-vector<vector<string>> partition(string s) {
+vector<vector<string>> partition(string s){
     vector<vector<string>> ans;
     vector<string> ds;
-    solve(0, s, ds, ans);
+    solve(0,s,ds,ans);
     return ans;
 }
