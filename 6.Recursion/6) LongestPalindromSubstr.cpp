d3ab36efcd859eac64da357d48d0d44acb671ab4
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-12 23:28:06 +0530

    Recursion

--- 6.Recursion/6) LongestPalindromSubstr.cpp
+++ 6.Recursion/6) LongestPalindromSubstr.cpp
@@ -12,7 +12,7 @@ void expand(string &s,int left,int right,int &maxl,int &start){
     }
 }
 string longestPalindrome(string s){
-    if (s.size() <= 1) return s;
+    if(s.size()<=1) return s;
     int maxl = 0,start = 0;
     
     for(int i=0;i<s.size();i++){
