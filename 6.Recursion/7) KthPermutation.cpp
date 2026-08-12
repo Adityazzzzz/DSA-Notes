684f2507890735bf24af6dc909e352cb5a9442f7
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-12 22:56:47 +0530

    Recursion

--- 6.Recursion/7) KthPermutation.cpp
+++ 6.Recursion/7) KthPermutation.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 using namespace std;
 
-string getPermutation(int n, int k) {
+string getPermutation(int n,int k){
     int fact = 1;
     vector<int> numbers;
-    for (int i = 1; i < n; i++) {
+    for(int i=1;i<n;i++){
         fact = fact * i;
         numbers.push_back(i);
     }
     numbers.push_back(n);
     string ans = "";
     k = k - 1;
-    while (true) {
+    while(true){
         int index = k / fact;
         ans = ans + to_string(numbers[index]);
         numbers.erase(numbers.begin() + index);
 
-        if (numbers.size() == 0) {
+        if(numbers.size() == 0){
             break;
         }
         
