6845e0c5950daf44ee7a53481413e927256c35ed
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-13 06:34:40 +0530

    S&Q

--- 9.Stack and Queues/13) SWmaximum.cpp
+++ 9.Stack and Queues/13) SWmaximum.cpp
@@ -5,7 +5,7 @@ vector<int>maxSlidingWindow(vector<int>&nums,int k){
     deque<int>dq;
     vector<int>ans;
 
-    for(int i=0; i<nums.size();i++){
+    for(int i=0;i<nums.size();i++){
         if(!dq.empty() && dq.front()==i-k){
             dq.pop_front();
         }
