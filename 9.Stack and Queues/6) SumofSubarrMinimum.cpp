c66cd49e88c0a769e16d8204936bb0ee7249bc90
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-13 06:12:59 +0530

    S&Q

--- 9.Stack and Queues/6) SumofSubarrMinimum.cpp
+++ 9.Stack and Queues/6) SumofSubarrMinimum.cpp
@@ -5,10 +5,11 @@ vector<int> getPSE(vector<int>& arr){
     int n = arr.size();
     vector<int> psee(n);
     stack<int> st;
-    for(int i = 0; i < n; i++){
-        while(!st.empty() && arr[st.top()] > arr[i])
-            st.pop();
 
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()] > arr[i]){
+            st.pop();
+        }
         psee[i] = st.empty() ? -1 : st.top();
         st.push(i);
     }
@@ -19,10 +20,11 @@ vector<int> getNSE(vector<int>& arr){
     int n = arr.size();
     vector<int> nse(n);
     stack<int> st;
-    for(int i = n - 1; i >= 0; i--){
-        while(!st.empty() && arr[st.top()] >= arr[i])
-            st.pop();
 
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()] >= arr[i]){
+            st.pop();
+        }
         nse[i] = st.empty() ? n : st.top();
         st.push(i);
     }
@@ -35,7 +37,7 @@ int sumSubarrayMins(vector<int>& arr){
     vector<int> nse = getNSE(arr);
     long long ans = 0;
 
-    for(int i = 0; i < n; i++){
+    for(int i=0;i<n;i++){
         long long left = i - psee[i];
         long long right = nse[i] - i;
 
