0b8ce01ae3e99e89f3f1850ab3515a0ec4f227a8
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-13 05:53:38 +0530

    GA

--- 12.Greedy Algorithms/8) ShortestJobFirst.cpp
+++ 12.Greedy Algorithms/8) ShortestJobFirst.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 long long solve(vector<int>& bt){
-    sort(bt.begin(), bt.end());
+    sort(bt.begin(),bt.end());
+
     long long wait = 0;
     long long curr = 0;
     for(int x:bt){
         wait += curr;
         curr += x;
     }
+    
     return wait / bt.size();
 }
