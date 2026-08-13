17fa3816cda45a6d76d32ab3ed9a8b0984ce66f5
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-13 06:02:48 +0530

    GA

--- 9.Stack and Queues/3) MinStack.cpp
+++ 9.Stack and Queues/3) MinStack.cpp
@@ -4,14 +4,14 @@ using namespace std;
 // tc: O(1)
 // sc:O(2n)
 class Minstack{
-    stack<pair<int, int>> st;
+    stack<pair<int,int>> st;
 
     void push(int val){
-        if (st.empty()){
-            st.push({val, val}); // (12,12)
+        if(st.empty()){
+            st.push({val,val}); // (12,12)
         }
         else{
-            st.push({val, min(val, st.top().second)}); // (12,12),(15,12),(10,10);
+            st.push({val, min(val,st.top().second)}); // (12,12),(15,12),(10,10);
         }
     }
     void getMin(){
@@ -23,35 +23,35 @@ class Minstack{
 };
 //----------------------------------------------------------------------------------------------
 
-class MinStack {
+class MinStack{
     stack<long long>st;
     long long mn;
 public:
-    void push(int val) {
-        if(st.empty()) {
+    void push(int val){
+        if(st.empty()){
             st.push(val);
             mn = val;
         }
-        else if(val>=mn) {
+        else if(val>=mn){
             st.push(val);
         }
-        else {
-            st.push(2LL*val-mn);
+        else{
+            st.push(2LL*val - mn);
             mn = val;
         }
     }
 
-    void pop() {
-        if(st.top()<mn) mn = 2LL*mn-st.top();
+    void pop(){
+        if(st.top()<mn) mn = 2LL*mn - st.top();
         st.pop();
     }
 
-    int top() {
+    int top(){
         if(st.top()<mn) return mn;
         return st.top();
     }
 
-    int getMin() {
+    int getMin(){
         return mn;
     }
 };
