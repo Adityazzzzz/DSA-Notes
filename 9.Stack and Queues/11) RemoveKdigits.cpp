5dd2f87ea534f52da1a76e401e649cc37dfe268d
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-13 06:31:16 +0530

    S&Q

--- 9.Stack and Queues/11) RemoveKdigits.cpp
+++ 9.Stack and Queues/11) RemoveKdigits.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 
-// took string in place of stack -> top=back here
-string removeKdigits(string num, int k){
+string removeKdigits(string num,int k){
     string st;
     for(auto it : num){
         while(!st.empty() && k>0 && st.back()>it){
@@ -11,14 +10,15 @@ string removeKdigits(string num, int k){
         }
         st.push_back(it);
     }
-    // Remove from end if k still remains
     while(k>0){
         st.pop_back();
         k--;
     }
-    // Remove leading zeros
+    
     int i = 0;
-    while(i<st.size() && st[i]=='0') i++;
+    while(i<st.size() && st[i]=='0'){
+        i++;
+    }
         
     string ans = st.substr(i);
     return ans.empty() ? "0" : ans;
