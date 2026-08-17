fb13910f5894df525b22ae9f5e9e86928babc989
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-17 12:09:23 +0530

    DP

--- 7.Dynamic Programming/2) Ques.cpp
+++ 7.Dynamic Programming/2) Ques.cpp
@@ -8,18 +8,17 @@ int solve(int n,vector<int> &dp){
         
     return dp[n]=solve(n-1,dp)+solve(n-2,dp);
 }
-int climbStairs(int n) {.
+int climbStairs(int n){.
     vector<int> dp(n+1,-1);
     return solve(n,dp);
 }
 
-int climbStairs(int n) {
-    if (n <= 2) return n;
-
+int climbStairs(int n){
+    if(n<=2) return n;
     int prev1 = 2;
     int prev2 = 1;
     int cur = 0;
-    for (int i = 3; i < n; i++) {
+    for(int i=3;i<n;i++){
         cur = prev1 + prev2;
         prev2 = prev1;
         prev1 = cur;
