6ae10edbfe981a743b76995cbc0599ada880f021
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-24 17:52:21 +0530

    DP

--- 7.Dynamic Programming/3) NinjaTraining.cpp
+++ 7.Dynamic Programming/3) NinjaTraining.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int func(int day,int last,vector<vector<int>&points,vector<vector<int>&dp){
+int func(int day,int last,vector<vector<int>&arr,vector<vector<int>&dp){
     if(day==0){
         int maxi = 0;
         for(int i=0;i<3;i++){
             if(i!=last){
-                maxi = max(maxi,points[0][i]);
+                maxi = max(maxi,arr[0][i]);
             }
         }
         return maxi;
@@ -17,7 +17,7 @@ int func(int day,int last,vector<vector<int>&points,vector<vector<int>&dp){
     int maxi = 0;
     for(int i=0;i<3;i++){
         if(i!=last){
-            int score = points[day][i] + func(day-1,i,points,dp);
+            int score = arr[day][i] + func(day-1,i,arr,dp);
             maxi = max(maxi,score);
         }
     }
@@ -25,19 +25,19 @@ int func(int day,int last,vector<vector<int>&points,vector<vector<int>&dp){
 }
 
 // tabulation ---------------------------------------------------------------------------------------
-int ninjatraining(int n,vector<vector<int>&points){
+int ninjatraining(int n,vector<vector<int>&arr){
     vector<vector<int>>dp(n,vector<int>(4,0));
 
-    dp[0][0] = max(points[0][1],points[0][2]);
-    dp[0][1] = max(points[0][0],points[0][2]);
-    dp[0][2] = max(points[0][0],points[0][1]);
-    dp[0][3] = max(points[0][0],max(points[0][1],points[0][2]));
+    dp[0][0] = max(arr[0][1],arr[0][2]);
+    dp[0][1] = max(arr[0][0],arr[0][2]);
+    dp[0][2] = max(arr[0][0],arr[0][1]);
+    dp[0][3] = max(arr[0][0],max(arr[0][1],arr[0][2]));
 
     for(int day=1;day<n;day++){
         for(int last=0;last<4;last++){
             for(int task=0;task<3;task++){
                 if(task != last){
-                    int score = points[day][task] + dp[day-1][task];
+                    int score = arr[day][task] + dp[day-1][task];
                     
                     dp[day][last] = max(dp[day][last],score);
                 }
@@ -48,20 +48,20 @@ int ninjatraining(int n,vector<vector<int>&points){
 }
 
 // space optimization -----------------------------------------------------------------------------------
-int ninjatraining(int n,vector<vector<int>&points){
+int ninjatraining(int n,vector<vector<int>&arr){
     vector<int>prev(4);
 
-    prev[0] = max(points[0][1],points[0][2]);
-    prev[1] = max(points[0][0],points[0][2]);
-    prev[2] = max(points[0][0],points[0][1]);
-    prev[3] = max(points[0][0],max(points[0][1],points[0][2]));
+    prev[0] = max(arr[0][1],arr[0][2]);
+    prev[1] = max(arr[0][0],arr[0][2]);
+    prev[2] = max(arr[0][0],arr[0][1]);
+    prev[3] = max(arr[0][0],max(arr[0][1],arr[0][2]));
 
     for(int day=1;day<n;day++){
         vector<int> temp(4,0);
         for(int last=0;last<4;last++){
             for(int task=0;task<3;task++){
                 if(task != last){
-                    temp[last] = max(temp[last],points[day][task] + prev[task]);
+                    temp[last] = max(temp[last],arr[day][task] + prev[task]);
                 }
             }
         }
