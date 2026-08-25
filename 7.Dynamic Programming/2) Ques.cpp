a323c31d73de7756aa5f1d5224cfcf63efeb4e77
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-25 07:24:26 +0530

    DP

--- 7.Dynamic Programming/2) Ques.cpp
+++ 7.Dynamic Programming/2) Ques.cpp
@@ -108,9 +108,11 @@ func(int index,vector<int>&dp){
 func(int index){
     vector<int>dp(n,0);
     dp[0] = arr[0];
-    for(int i=1;i<n;i++){
-        int pick = arr[index] + dp[n-2];
-        int notpick = 0 + dp[n-1];
+    dp[1] = max(arr[0],arr[1]);
+
+    for(int i=2;i<n;i++){
+        int pick = arr[index] + dp[i-2];
+        int notpick = 0 + dp[i-1];
 
         dp[i] = max(pick,notpick);
     }
