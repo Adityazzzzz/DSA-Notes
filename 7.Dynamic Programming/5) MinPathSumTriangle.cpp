e8b973c82e9cabc6ee4fef149d700eef3b472d94
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-25 07:44:09 +0530

    DP

--- 7.Dynamic Programming/5) MinPathSumTriangle.cpp
+++ 7.Dynamic Programming/5) MinPathSumTriangle.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int f(int i, int j, vector<vector<int>>& triangle, int n) {
-    if(i == n-1) {
-        return triangle[n - 1][j];
+int f(int i,int j,vector<vector<int>>& triangle,int n){
+    if(i == n-1){
+        return triangle[n-1][j];
     }
     if(dp[i][j] == -1) return dp[i][j];
 
     int down = triangle[i][j] + f(i+1,j,triangle,n);
     int diagonal = triangle[i][j] + f(i+1,j+1,triangle,n);
 
-    return dp[i][j] =min(down, diagonal);
+    return dp[i][j] =min(down,diagonal);
 }
 // ------------------------------------------------------------------------------------------------
 // tabulation
-int f(int i, int j, vector<vector<int>>& triangle, int n) {
+int f(int i,int j,vector<vector<int>>& triangle,int n){
     int dp[n][n] = {0};
 
-    for(int j=0;j<n;j++) dp[n-1][j] = triangle[n-1][j];
+    for(int j=0;j<n;j++){
+        dp[n-1][j] = triangle[n-1][j];
+    }
 
     for(int i=n-2;i>=0;i--){
         for(int j=0;j<=i;j++){
@@ -31,10 +33,12 @@ int f(int i, int j, vector<vector<int>>& triangle, int n) {
 }
 //-------------------------------------------------------------------------------------------------
 // space optimization
-int f(vector<vector<int>>& triangle, int n) {
+int f(vector<vector<int>>& triangle,int n){
     vector<int>front(n,0);
 
-    for(int j=0;j<n;j++) front[j] = triangle[n-1][j];
+    for(int j=0;j<n;j++){
+        front[j] = triangle[n-1][j];
+    }
 
     for(int i=n-2;i>=0;i--){
         vector<int>curr(n,0);
@@ -51,27 +55,27 @@ int f(vector<vector<int>>& triangle, int n) {
 
 
 //----------------------------------------------------------------------------------
-// QUESTION: Min/Max in Path Sum , here we can move leftdiagonal,righrdiagonal and down
+// QUESTION: Min/Max in Path Sum ,here we can move leftdiagonal,righrdiagonal and down
 call functions by
 maxi = max(maxi,f(n-1,j,matrix));
 
-int f(int i, int j, vector<vector<int>> &matrix){
+int f(int i,int j,vector<vector<int>> &matrix){
     if (j < 0 || j >= matrix[0].size()) return -1e8;
     if (i == 0) return matrix[0][j];
     if(dp[i][j] == -1) return dp[i][j];
 
-    int u = matrix[i][j] + f(i - 1, j, matrix);
-    int ld = matrix[i][j] + f(i - 1, j - 1, matrix);
-    int rd = matrix[i][j] + f(i - 1, j + 1, matrix);
+    int u = matrix[i][j] + f(i-1,j,matrix);
+    int ld = matrix[i][j] + f(i-1,j-1,matrix);
+    int rd = matrix[i][j] + f(i-1,j + 1,matrix);
 
-    return dp[i][j] = max(u, max(ld, rd));
+    return dp[i][j] = max(u,max(ld,rd));
 }
 
 //tabulation
 int getMaxPathSum(vector<vector<int>> &matrix){
     int n = matrix.size();
     int m = matrix[0].size();
-    vector<vector<int>> dp(n, vector<int>(m, 0));
+    vector<vector<int>> dp(n,vector<int>(m,0));
 
     for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
 
