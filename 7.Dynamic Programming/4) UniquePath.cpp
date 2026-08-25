5830a1922b25714ff2a947c1c59aa2879eeb217f
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-25 07:23:28 +0530

    DP

--- 7.Dynamic Programming/4) UniquePath.cpp
+++ 7.Dynamic Programming/4) UniquePath.cpp
@@ -6,10 +6,11 @@ int func(int i,int j,vector<vector<int>>&mat,vector<vector<int>>&dp){
     if(i>=0 && j>=0 && mat[i][j]==-1) return 0;
     if(i==0 && j==0) return 1;
     if(i<0 || j<0) return 0;
+    
     if(dp[i][j]!=-1) return dp[i][j];
 
     int up = func(i-1,j,mat);
-    int down = func(i,j-1,mat);
+    int left = func(i,j-1,mat);
     
     return dp[i][j] = up+left % mod;
 } 
