4190cc650cb0f2be076c80542f7ce68c179d5c3e
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-26 13:20:13 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/7) S-2.cpp
+++ 7.Dynamic Programming/DP-onLCS/7) S-2.cpp
@@ -14,7 +14,7 @@ bool func(int ind,int tar,vector<int>& arr){
 }
 
 // dp
-bool func(int ind,int tar,vector<int>& arr,vector<vector<int>>& dp) {
+bool func(int ind,int tar,vector<int>& arr,vector<vector<int>>& dp){
     if(tar==0) return true;
     if(ind==0) return arr[0] == tar;
     if(dp[ind][tar]!=-1) return dp[ind][tar];
@@ -26,6 +26,27 @@ bool func(int ind,int tar,vector<int>& arr,vector<vector<int>>& dp) {
     return dp[ind][tar] = take | notTake;
 }
 
+// tabulation
+bool subsetSumToK(int n,int k,vector<int>& arr){
+    vector<vector<bool>> dp(n,vector<bool>(k + 1,false));
+    for(int i=0;i<n;i++){
+        dp[i][0] = true;
+    }
+    if(arr[0]<=k) dp[0][arr[0]] = true;
+
+    for(int ind=1;ind<n;ind++){
+        for(int tar=1;tar<=k;tar++){
+            bool notTake = dp[ind-1][tar];
+            bool take = false;
+            if(arr[ind] <= tar) take = dp[ind-1][tar-arr[ind]];
+
+            dp[ind][tar] = take | notTake;
+        }
+    }
+    return dp[n-1][k];
+}
+
+// space optimization
 bool func(int n,int k,vector<int> &arr){
     vector<bool> prev(k+1,0),cur(k+1,0);
     prev[0] = cur[0] = true;
