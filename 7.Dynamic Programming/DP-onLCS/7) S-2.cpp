f94cd67d9425457ca353e8f4d59dd63e76ec053f
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-26 13:16:07 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/7) S-2.cpp
+++ 7.Dynamic Programming/DP-onLCS/7) S-2.cpp
@@ -1,19 +1,44 @@
 #include<iostream>
 using namespace std;
 
-bool subsetSumToK(int n,int k,vector<int> &arr){
+// recursive
+bool func(int ind,int tar,vector<int>& arr){
+    if(tar==0) return true;
+    if(ind==0) return arr[0] == tar;
+
+    bool notTake = func(ind-1,tar,arr);
+    bool take = false;
+    if(arr[ind]<=tar) take = func(ind-1,tar-arr[ind],arr);
+
+    return take | notTake;
+}
+
+// dp
+bool func(int ind,int tar,vector<int>& arr,vector<vector<int>>& dp) {
+    if(tar==0) return true;
+    if(ind==0) return arr[0] == tar;
+    if(dp[ind][tar]!=-1) return dp[ind][tar];
+
+    bool notTake = func(ind-1,tar,arr,dp);
+    bool take = false;
+    if(arr[ind]<=tar) take = func(ind-1,tar-arr[ind],arr,dp);
+
+    return dp[ind][tar] = take | notTake;
+}
+
+bool func(int n,int k,vector<int> &arr){
     vector<bool> prev(k+1,0),cur(k+1,0);
     prev[0] = cur[0] = true;
     prev[arr[0]] = true;
 
     for(int ind=1;ind<n;ind++){
-        for(int target=1;target<=k;target++){
-            bool notTake = prev[target];
+        for(int tar=1;tar<=k;tar++){
+            bool notTake = prev[tar];
 
             bool take = false;
-            if(arr[ind] <= target) take = prev[target-arr[ind]];
+            if(arr[ind] <= tar) take = prev[tar-arr[ind]];
 
-            cur[target] = take | notTake;
+            cur[tar] = take | notTake;
         }
         prev = cur;
     }
@@ -29,5 +54,5 @@ bool canPartition(vector<int> &arr,int n){
     if(totSum%2==1) return false;
     int k = totSum/2;
 
-    return subsetSumToK(n,k,arr);
+    return func(n,k,arr);
 }
