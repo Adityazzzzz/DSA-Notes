22e235e2f5e33e56fe1b1407bbf225de50024f24
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-26 16:48:51 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/10) MinimumCoins.cpp
+++ 7.Dynamic Programming/DP-onLCS/10) MinimumCoins.cpp
@@ -1,59 +1,58 @@
 #include<iostream>
 using namespcae std
 
-int f(int ind,int T,vector<int> &nums){
+int f(int ind,int k,vector<int> &nums){
     if(ind == 0){
-        if(T % nums[0] == 0) return T/nums[0];
+        if(k % nums[0] == 0) return k/nums[0];
         return 1e9;
     }
-    if(dp[ind][T] != -1) return dp[ind][T];
+    if(dp[ind][k] != -1) return dp[ind][k];
 
-    int notTake = 0 + f(ind-1,T,nums);
+    int notTake = 0 + f(ind-1,k,nums);
     int take = INT_MAX;
-    if(nums[ind]<=T) take = 1 + f(ind,T-nums[ind],nums);
+    if(nums[ind]<=k) take = 1 + f(ind,k-nums[ind],nums);
 
-    return dp[ind][T] = max(take,notTake);
+    return dp[ind][k] = max(take,notTake);
 }
 
 // tabulation --------------------------------------------------------------------------------------
-int minimumCoins(vector<int>& nums,int tar){
+int minimumCoins(vector<int>& nums,int k){
     int n = nums.size();
-    vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    vector<vector<int>> dp(n,vector<int>(k+1,0));
 
-    for(int T=0;T<=tar;T++){
-        if(T % nums[0] == 0) dp[0][T] = T/nums[0];
-        else dp[0][T] = 1e9;
+    for(int i=0;i<=k;i++){
+        if(i % nums[0] == 0) dp[0][i] = i/nums[0];
+        else dp[0][i] = 1e9;
     }
 
     for(int ind=1;ind<n;ind++){
-        for(int T=0;T<=tar;T++){
+        for(int tar=0;tar<=k;tar++){
 
-            int notTake = dp[ind-1][T];
+            int notTake = dp[ind-1][tar];
             int take = INT_MAX; 
-            if(nums[ind]<=T) take = 1 + dp[ind][T-nums[ind]];
+            if(nums[ind]<=tar) take = 1 + dp[ind][tar-nums[ind]];
             
-            dp[ind][T] = min(take,notTake);
+            dp[ind][tar] = min(take,notTake);
         }
     }
-    int ans = dp[n-1][tar];
+    int ans = dp[n-1][k];
     if(ans >= 1e9) return -1;
     return ans;
 }
 
 // space optimization -----------------------------------------------------------------------------------
-int minimumCoinsSpaceOptimized(vector<int>& nums,int tar){
+int minimumCoinsSpaceOptimized(vector<int>& nums,int k){
     int n = nums.size();
-    vector<int>dp(tar+1,1e9);
+    vector<int>dp(k+1,1e9);
     dp[0] = 0;
 
-    for(int i=0; i<n;++i){
-        for(int j = nums[i]; j<=tar; ++j){
+    for(int i=0;i<n;++i){
+        for(int j=nums[i];j<=k;++j){
             if(dp[j-nums[i]] != 1e9){
                 dp[j] = min(dp[j],1 + dp[j-nums[i]]);
             }
         }
     }
-
-    if(dp[tar] >= 1e9) return -1;
-    return dp[tar];
+    if(dp[k] >= 1e9) return -1;
+    return dp[k];
 }
