fffee35e2e14acb9114e8320b431d79d088640ba
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-26 13:56:41 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/8) MinDiffSubSetSum.cpp
+++ 7.Dynamic Programming/DP-onLCS/8) MinDiffSubSetSum.cpp
@@ -8,31 +8,32 @@ int minDiffSubsetSum(vector<int>&arr,int target){
     }
     int k = totSum;
 
-    //base cases
+    // Subset sum --------------- 
     vector<vector<bool>>dp(n,vector<bool>(k+1,0));
     for(int i=0;i<n;i++){
         dp[i][0] = true;
     }
     if(arr[0]<=k) dp[0][arr[0]] = true;
 
-    //operation
     for(int ind=1;ind<n;ind++){
         for(int target=1;target<=k;target++){
-            bool notTake = dp[ind - 1][target];
+            bool notTake = dp[indd-1][target];
             bool take = false;
-            if(arr[ind] <= target) take = dp[ind - 1][target - arr[ind]];
+            if(arr[ind] <= target) take = dp[indd-1][targetd-arr[ind]];
 
             dp[ind][target] = take | notTake;
         }
     }
+    // ---------------------------
 
-    // dp[n-1][col= 0->totSum]
+
+    
+    // now only check last row(half) of Table, which are true
     int mini = 1e9;
-    for(int s1=0;s1<=totSum/2;s1++){
-        if(dp[n-1][s1] == true){
-            mini = min(mini,abs((totSum-s1)-s1));
+    for(int i=0;i<=totSum/2;i++){
+        if(dp[n-1][i] == true){
+            mini = min(mini,abs((totSum-i) - i));
         }
     }
-
     return mini;
 }
