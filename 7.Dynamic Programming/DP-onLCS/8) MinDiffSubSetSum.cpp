57464267b8d1790818d9cf677767db994c594971
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-26 14:06:54 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/8) MinDiffSubSetSum.cpp
+++ 7.Dynamic Programming/DP-onLCS/8) MinDiffSubSetSum.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int minDiffSubsetSum(vector<int>&arr,int target){
+int minDiffSubsetSum(vector<int>&arr,int tar){
     int totSum = 0;
     for(int i=0;i<n;i++){
         totSum += arr[i];
@@ -16,18 +16,18 @@ int minDiffSubsetSum(vector<int>&arr,int target){
     if(arr[0]<=k) dp[0][arr[0]] = true;
 
     for(int ind=1;ind<n;ind++){
-        for(int target=1;target<=k;target++){
-            bool notTake = dp[indd-1][target];
+        for(int tar=1;tar<=k;tar++){
+            bool notTake = dp[ind-1][tar];
             bool take = false;
-            if(arr[ind] <= target) take = dp[indd-1][targetd-arr[ind]];
+            if(arr[ind] <= tar) take = dp[ind-1][tar-arr[ind]];
 
-            dp[ind][target] = take | notTake;
+            dp[ind][tar] = take | notTake;
         }
     }
     // ---------------------------
 
 
-    
+
     // now only check last row(half) of Table, which are true
     int mini = 1e9;
     for(int i=0;i<=totSum/2;i++){
