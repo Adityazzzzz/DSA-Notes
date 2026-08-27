ef17e7899fb9b83a77889867b1804052f9dc7bf9
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-27 14:18:22 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/18) EditDist.cpp
+++ 7.Dynamic Programming/DP-onLCS/18) EditDist.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int f(int i,intj,string &s1,string &s2,vector<vector<int>> &dp){
+int f(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
     if(i<0) return j+1; 
     if(j<0) return i+1; 
     if(dp[i][j]!=-1) return dp[i][j];
 
     if(s1[i]==s2[j]) return f(i-1,j-1,s1,s2,dp); 
 
-    return dp[i][j] = 1 + min(f(i-1,j,s1,s2,dp), 
-                                min( f(i,j-1,s1,s2,dp), 
-                                    f(i-1,j-1,s1,s2,dp))); 
+    return dp[i][j] = 1 + min({
+                            f(i-1,j,s1,s2,dp),
+                            f(i,j-1,s1,s2,dp),
+                            f(i-1,j-1,s1,s2,dp)
+                          });  
 }
 
 // same code after shifting
@@ -20,7 +22,64 @@ int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
     if(dp[i][j]!=-1) return dp[i][j];
 
     if(s1[i-1] == s2[j-1]) return f(i-1,j-1,s1,s2,dp); 
-    return dp[i][j] = 1 + min(f(i-1,j,s1,s2,dp),min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp))); 
+    return dp[i][j] = 1 + min({
+                            f(i-1,j,s1,s2,dp),
+                            f(i,j-1,s1,s2,dp),
+                            f(i-1,j-1,s1,s2,dp)
+                          }); 
 }
 
+// tabulation -----------------------------------------------------------------------------
+int minDistance(string s1,string s2){
+    int n = s1.size();
+    int m = s2.size();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
+    for(int i=0;i<=n;i++){
+        dp[i][0] = i;
+    }
+    for(int j=0;j<=m;j++){
+        dp[0][j] = j;
+    }
+        
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+
+            if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
+            else{
+                int insert = dp[i][j-1];
+                int del = dp[i-1][j];
+                int replace = dp[i-1][j-1];
+
+                dp[i][j] = 1 + min({insert,del,replace});
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+// space optimization ----------------------------------------------------------------------------
+int minDistance(string s1,string s2){
+    int n = s1.size();
+    int m = s2.size();
+    vector<int> prev(m+1);
+    vector<int> curr(m+1);
+
+    for(int j=0;j<=m;j++) prev[j] = j;
+
+    for(int i=1;i<=n;i++){
+        curr[0] = i; // imp line
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]) curr[j] = prev[j-1];
+            else{
+                int insert = curr[j-1];
+                int del = prev[j];
+                int replace = prev[j-1];
+
+                curr[j] = 1 + min({insert,del,replace});
+            }
+        }
+        prev = curr;
+    }
+    return prev[m];
+}
