42d04678ba5a7a9d231b52a3257a58f69a7833cf
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-27 15:08:14 +0530

    DP

--- 7.Dynamic Programming/DP-BuyandSellStocks/20) B&Sstocks1.cpp
+++ 7.Dynamic Programming/DP-BuyandSellStocks/20) B&Sstocks1.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices){
-        int minPrice = INT_MAX;
-        int maxProfit = 0;
-        for (int it : prices) {
-            minPrice = min(minPrice, it);
-            maxProfit = max(maxProfit, it - minPrice);
+    int maxProfit(vector<int>& arr){
+        int mini = INT_MAX;
+        int maxp = 0;
+        for(int it:arr){
+            mini = min(mini,it);
+            maxp = max(maxp,it-mini);
         }
-        return maxProfit;
+        return maxp;
     }
 };
