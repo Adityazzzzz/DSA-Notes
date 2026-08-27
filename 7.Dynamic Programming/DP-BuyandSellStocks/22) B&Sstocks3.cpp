a242786cd53b8a28712357064d4b670b3c5e4db9
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-27 15:54:15 +0530

    DP

--- 7.Dynamic Programming/DP-BuyandSellStocks/22) B&Sstocks3.cpp
+++ 7.Dynamic Programming/DP-BuyandSellStocks/22) B&Sstocks3.cpp
@@ -20,11 +20,13 @@ int maxProfit(vector<int>& prices, int n){
     for(int ind=n-1;ind>=0;ind--){
         for(int buy=0;buy<=1;buy++){
             for(int cap=1;cap<=2;cap++){
-                if (buy == 1) {
-                    dp[ind][buy][cap] = max(-prices[ind]+dp[ind+1][0][cap] ,0+dp[ind+1][1][cap]);
+                if(buy == 1){
+                    dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap], 
+                                            0 + dp[ind+1][1][cap]);
                 }
-                else {
-                    dp[ind][buy][cap] = max(prices[ind]+dp[ind+1][1][cap-1], 0+dp[ind+1][0][cap])
+                else{
+                    dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1], 
+                                            0 + dp[ind+1][0][cap])
                 }
             }
         }
