00b473a57c2d2ed6571b3853ee429722b06c82c7
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-27 14:02:01 +0530

    DP

--- 7.Dynamic Programming/DP-onLCS/17) DistinctSubSeq.cpp
+++ 7.Dynamic Programming/DP-onLCS/17) DistinctSubSeq.cpp
@@ -22,12 +22,8 @@ int numDistinct(string s,string t){
     int m = t.size();
     vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-    for(int i=0;i<=m;i++){
-        dp[0][i] = 0;
-    }
-    for(int i=0;i<=n;i++){
-        dp[i][0] = 0;
-    }
+    for(int i=0;i<=n;i++) dp[i][0] = 1;
+    for(int j=1;j<=m;j++) dp[0][j] = 0;
 
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
