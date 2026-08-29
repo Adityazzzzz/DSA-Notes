f9159394b681515fb616225a1e16b414222d1d6f
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-29 23:50:07 +0530

    Trie

--- 10.Trie/5) MaxXORusingTrie.cpp
+++ 10.Trie/5) MaxXORusingTrie.cpp
@@ -9,7 +9,7 @@ struct Node{
     Node* get(int bit){
         return links[bit];
     }
-    void put(int bit, Node* node){
+    void put(int bit,Node* node){
         links[bit] = node;
     }
 };
@@ -20,14 +20,13 @@ private:
     Trie(){
         root = new Node();
     }
-
 public:
     void insert(int num){
         Node* node = root;
-        for(int i = 31; i >= 0; i--){
-            int bit =(num >> i) & 1;
-            if(!node->containsKey(bit)){
-                node->put(bit, new Node());
+        for(int i=31;i>=0;i--){
+            int bit = (num>>i) & 1;
+            if(node->containsKey(bit) == false){
+                node->put(bit,new Node());
             }
             node = node->get(bit);
         }
@@ -35,8 +34,8 @@ public:
     int getMax(int num){
         Node* node = root;
         int maxNum = 0;
-        for(int i = 31; i >= 0; i--){
-            int bit =(num >> i) & 1;
+        for(int i=31;i>=0;i--){
+            int bit = (num>>i) & 1;
             if(node->containsKey(1-bit)){
                 maxNum = maxNum | (1 << i);
                 node = node->get(1-bit);
@@ -49,14 +48,14 @@ public:
     }
 };
 
-int maxXOR(int n, int m, vector<int> &arr1, vector<int> &arr2){
+int maxXOR(int n,int m,vector<int> &arr1,vector<int> &arr2){
     Trie trie;
-    for(auto &it : arr1){
+    for(auto it:arr1){
         trie.insert(it);
     }
     
     int maxi = 0;
-    for(auto &it : arr2){
+    for(auto it:arr2){
         maxi = max(maxi,trie.getMax(it));
     }
     return maxi;
