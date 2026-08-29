d267778b3b363346b68e4fe6c0f2b78b3fe3235c
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-29 08:54:18 +0530

    DP

--- 7.Dynamic Programming/DP-onLIS/23) LIS.cpp
+++ 7.Dynamic Programming/DP-onLIS/23) LIS.cpp
@@ -47,23 +47,24 @@ int longestIncreasingSubsequence(int arr[],int n){
 }
 
 int printlongestIncreasingSubsequence(int arr[],int n){
-    vector<int>dp(n,1);
+    vector<int>dp(n,1),hasharr(n);
     int maxi = 1;
     int lastInd = 0;
 
     for(int i=0;i<n;i++){
         hasharr[i]=i;
         for(int prev=0;prev<i;prev++){
-            if(arr[prev]<arr[i] && (1+dp[prev])>dp[i]){
-                dp[i] = 1+dp[i];
-                has[i] = prev;
+            if(arr[prev]<arr[i] && (1+dp[prev]) > dp[i]){
+                dp[i] = 1 + dp[i];
+                hasharr[i] = prev;
             }
         }
-        if(dp[i]>maxi){
+        if(dp[i] > maxi){
             maxi=dp[i];
             lastInd==i;
         }
     }
+
     vector<int>temp;
     temp.push_back(arr[lastInd]);
     while(hash[lastInd] != lastInd){
