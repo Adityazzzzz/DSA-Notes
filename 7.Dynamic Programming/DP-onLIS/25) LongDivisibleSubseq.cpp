d4d20fd9d1567a2963b86a5f8919a4bf3e26bb68
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-29 09:11:58 +0530

    DP

--- 7.Dynamic Programming/DP-onLIS/25) LongDivisibleSubseq.cpp
+++ 7.Dynamic Programming/DP-onLIS/25) LongDivisibleSubseq.cpp
@@ -10,7 +10,7 @@ int printlongestIncreasingSubsequence(int arr[],int n){
 
     for(int i=0;i<n;i++){
         hasharr[i]=i;
-        for(int prev=0;prev<i;prev++) {
+        for(int prev=0;prev<i;prev++){
             if(arr[prev] % arr[prev]==0 && (1+dp[prev]) > dp[i]){
                 dp[i] = 1 + dp[i];
                 hasharr[i] = prev;
