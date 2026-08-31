aeb76349c6a735308d32f3c8c89f3b4277909b2e
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-31 21:53:03 +0530

    Graph

--- 5.Graph/9) WordLadder-1.cpp
+++ 5.Graph/9) WordLadder-1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // word-ladder 1
-int wordladder(string startword, string targtword, vector<string>&wordlist){
+int wordladder(string startword,string targtword,vector<string>&wordlist){
     queue<pair<string,int>q,
     q.push({startword,1});
 
@@ -16,14 +16,14 @@ int wordladder(string startword, string targtword, vector<string>&wordlist){
 
         if(word == targetword) return steps;
 
-        for(int i = 0; i < word.size(); i++){
+        for(int i=0;i<word.size();i++){
             char original = word[i];
-            for(char ch = 'a'; ch <= 'z'; ch++){
+            for(char ch='a';ch<='z';ch++){
                 word[i] = ch;
 
                 if(st.find(word) != st.end()){
                     st.erase(word);
-                    q.push({word, steps + 1});
+                    q.push({word,steps + 1});
                 }
             }
             word[i] = original;
@@ -39,7 +39,7 @@ unordered_map<string,int>mpp;
 vector<vector<string>ans;
 string str;
 
-void dfs(string word, vector<string>&seq){
+void dfs(string word,vector<string>&seq){
     if(word==str){
         reverse(seq.begin(),seq.end());
         ans.push_back(seq);
@@ -47,9 +47,9 @@ void dfs(string word, vector<string>&seq){
         return;
     }
     int steps = mpp[word];
-    for(int i = 0; i < size; i++){
+    for(int i=0;i<size;i++){
         char original = word[i];
-        for(char ch = 'a'; ch <= 'z'; ch++){
+        for(char ch='a';ch<='z';ch++){
             word[i] = ch;
             if(mpp.find(word)!=mpp.end() && mpp[word]+1 == steps){
                 seq.push_back(word);
@@ -62,7 +62,7 @@ void dfs(string word, vector<string>&seq){
 
 }
 
-vector<vector<int>> findladder(string beginword, string endword, vector<string>&wordlist){
+vector<vector<int>> findladder(string beginword,string endword,vector<string>&wordlist){
     queue<string>q,
     q.push(beginword);
     str = beginword;
@@ -80,9 +80,9 @@ vector<vector<int>> findladder(string beginword, string endword, vector<string>&
         q.pop();
 
         if(word==endword) break;
-        for(int i = 0; i < size; i++){
+        for(int i=0;i<size;i++){
             char original = word[i];
-            for(char ch = 'a'; ch <= 'z'; ch++){
+            for(char ch='a';ch<='z';ch++){
                 word[i] = ch;
                 if(st.cound(word)){
                     q.push(word);
