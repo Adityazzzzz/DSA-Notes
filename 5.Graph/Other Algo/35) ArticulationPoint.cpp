807ef4aeb22281c7c263d5c075a1b1300a8878f9
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-31 16:04:19 +0530

    Graph

--- 5.Graph/Other Algo/35) ArticulationPoint.cpp
+++ 5.Graph/Other Algo/35) ArticulationPoint.cpp
@@ -6,22 +6,22 @@ void dfs(int u,int p,vector<int> &vis,vector<int> &tin,vector<int> &low,vector<i
     tin[u] = low[u] = timer++;
     int children = 0;
 
-    for (int v : adj[u]){
-        if (v == p) continue; 
-        if (!vis[v]){
+    for(int v:adj[u]){
+        if(v == p) continue; 
+        if(!vis[v]){
             dfs(v,u,vis,tin,low,mark,adj,timer);
             low[u] = min(low[u],low[v]);
 
-            if (low[v] >= tin[u] && p != -1){
+            if(low[v] >= tin[u] && p != -1){
                 mark[u] = 1; // articulation point
             }
             children++;
-        } 
+        }
         else{
             low[u] = min(low[u],tin[v]);
         }
     }
-    if (p == -1 && children > 1){
+    if(p == -1 && children > 1){
         mark[u] = 1;
     }
 }
@@ -30,8 +30,8 @@ vector<int> articulationPoints(int n,vector<int> adj[]){
     vector<int> vis(n,0),tin(n,-1),low(n,-1),mark(n,0);
     int timer = 0;
 
-    for (int i=0;i<n;i++){
-        if (!vis[i]){
+    for(int i=0;i<n;i++){
+        if(!vis[i]){
             dfs(i,-1,vis,tin,low,mark,adj,timer);
         }
     }
@@ -40,7 +40,7 @@ vector<int> articulationPoints(int n,vector<int> adj[]){
         if(mark[i] == 1) ans.push_back(i);
     }
 
-    if (ans.empty()) return{-1};
+    if(ans.empty()) return{-1};
     return ans;
 }
 
