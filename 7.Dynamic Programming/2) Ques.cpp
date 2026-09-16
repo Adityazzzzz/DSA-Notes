def7cd73b1a8d126ba7ad40d4fe3a2f804a70c55
Adityazzzzz <adityasinghrajawat2004@gmail.com>
2026-09-16 17:16:03 +0530

    DP

--- 7.Dynamic Programming/2) Ques.cpp
+++ 7.Dynamic Programming/2) Ques.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int solve(int n,vector<int> &dp){
     if(n<=2) return n;
     if(dp[n]!=-1) return dp[n]; 
-        
+
     return dp[n] = solve(n-1,dp) + solve(n-2,dp);
 }
 int climbStairs(int n){.
